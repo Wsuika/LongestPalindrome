6bd5aa035d114794608a425bce8319d26950c842
Wsuika <wsuika.dev@gmail.com>
2020-08-16 19:38:51 +0800

    Initial Commit

---
+++ LongestPalindrome/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LongestPalindrome
+//
+//  Created by melon on 8/16/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
